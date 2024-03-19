e52e91388e75d12c7ce599d51278ae94254a4209
dapedrozo <lanceroexperto@gmail.com>
2024-03-19 11:47:59 -0500

    first commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -11,7 +11,7 @@ export default {
         "mongodb://127.0.0.1:27017/ia",
     },
     FRONTENDURL: process.env.FRONTEND_URL || "http://localhost:5173",
-    API_KEY: process.env.API_KEY_DEFAULT || "API_DEFAULT",
+    API_KEY: process.env.API_KEY_EMPRESA_DEFAULT || "API_DEFAULT",
     ID_ASSISTANT: process.env.ID_ASSISTANT_DEFAULT || "ASSISTANT_DEFAULT",
     IDENTIFICACION: process.env.IDENTIFICACION_DEFAULT || "000"
   };
